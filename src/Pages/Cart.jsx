e10114797332a77edf66626b389760e28be0b4cb
Raiyan-Nawaz <raiyannawaz95@gmail.com>
2024-07-09 14:33:10 +0530

    final commit

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -85,10 +85,10 @@ const Cart = () => {
                 }
             }
             else if (input.value === '') {
-                text.innerHTML = `<span class="text-danger">Please Write Something</span><svg class="mx-0 my-1 text-danger MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ClearIcon"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>`
+                text.innerHTML = `<span class="text-danger">Please Write Something !</span>`
             }
             else {
-                text.innerHTML = `<span class="text-danger">Invalid   Code</span><svg class="mx-0 my-1 text-danger MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ClearIcon"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>`
+                text.innerHTML = `<span class="text-danger">Invalid Code !</span>`
             }
         }
     }
